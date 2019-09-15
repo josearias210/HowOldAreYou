3d4110b3c70b21304c43d02256f890c5b20b4590
Jose Antonio Arias Rodriguez <josearias210@gmail.com>
2019-09-15 14:53:02 -0400

    Carga inicial

---
+++ HowOldAreyou/HowOldAreyou/Services/FaceService.cs
@@ -0,0 +1,66 @@
+﻿namespace HowOldAreyou.Services
+{
+    using HowOldAreyou.Models;
+    using Newtonsoft.Json;
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+
+    public class FaceService
+    {
+        private static readonly string subscriptionKey = "68933cde15aa48f98568c1b35f8ef96c";
+        //private static string uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+        private static string uriBase = "https://dev-josearias210.cognitiveservices.azure.com/face/v1.0/detect";
+        public async Task<double> DetectAge(string imageFilePath)
+        {
+            double age = 0;
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                string requestParameters = "returnFaceId=true&returnFaceLandmarks=false" +
+                    "&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses," +
+                    "emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+                string uri = uriBase + "?" + requestParameters;
+
+                HttpResponseMessage response;
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+                using (ByteArrayContent content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                    response = await client.PostAsync(uri, content);
+                    string json = await response.Content.ReadAsStringAsync();
+                    var faceResult = JsonConvert.DeserializeObject<FaceResult[]>(json);
+                    if (faceResult != null && faceResult.Length > 0)
+                    {
+                        age = faceResult[0].faceAttributes.age;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return age;
+        }
+
+        private byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+
+
+    }
+}
---
+++ HowOldAreyou/HowOldAreyou/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,183 @@
+﻿namespace HowOldAreyou.ViewModels
+{
+    using HowOldAreyou.Services;
+    using Plugin.Media;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+    public class MainPageViewModel : ViewModelBase
+    {
+        #region  Attributes
+
+        private int age;
+        private bool isBusy;
+        private decimal score;
+        private string pathImage;
+        private bool isLoadPhoto;
+        private ImageSource photo;
+
+        #endregion
+
+        #region Properties
+
+        public ImageSource Photo
+        {
+            get { return photo; }
+            set
+            {
+                photo = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsLoadPhoto
+        {
+            get { return isLoadPhoto; }
+            set
+            {
+                isLoadPhoto = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged();
+            }
+        }
+        public decimal Score
+        {
+            get { return score; }
+            set
+            {
+                score = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set
+            {
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Service
+
+        private FaceService service;
+
+        #endregion
+
+        #region Commands
+
+        public ICommand TakePhotoCommand { get; }
+        public ICommand PickPhotoCommand { get; }
+        public ICommand AnalyzeCommand { get; }
+
+        #endregion
+
+        #region Constructs
+        public MainPageViewModel()
+        {
+            service = new FaceService();
+            PickPhotoCommand = new Command(async () => await PickPhoto());
+            TakePhotoCommand = new Command(async () => await TakePhoto());
+            AnalyzeCommand = new Command(async () => await Analyze());
+        }
+
+
+        #endregion
+
+        #region Private
+
+        private async Task Analyze()
+        {
+            if (Photo == null || string.IsNullOrEmpty(pathImage) || pathImage == "default.jpg")
+            {
+                await Application.Current.MainPage.DisplayAlert("Xamarin Latino", "No has seleccionado una foto", "Ok");
+                return;
+            }
+
+            IsBusy = true;
+            try
+            {
+                var age = await service.DetectAge(pathImage);
+                if (age > 0)
+                {
+                    this.Age = (int)age;
+                }
+
+            }
+            catch
+            {
+                await Application.Current.MainPage.DisplayAlert("Xamarin Latino", "No se pudo evaluar la foto", "Ok");
+            }
+            IsBusy = false;
+
+        }
+
+        private async Task PickPhoto()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+            });
+
+            if (file == null)
+                return;
+
+
+            Photo = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                pathImage = file.Path;
+                IsLoadPhoto = true;
+                return stream;
+            });
+        }
+
+        private async Task TakePhoto()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "XamarinLatino",
+                Name = "age.jpg"
+            });
+
+            if (file == null)
+                return;
+
+
+            Photo = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                pathImage = file.Path;
+                IsLoadPhoto = true;
+                return stream;
+            });
+        }
+        #endregion
+
+    }
+}
